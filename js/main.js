54a4501bc58308681feb4a8ab70fe8d2259fc1fe
physicstjc <85329791+physicstjc@users.noreply.github.com>
2025-03-31 21:40:30 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('/data/simulations.xml')
+    fetch('data/simulations.xml')
         .then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
